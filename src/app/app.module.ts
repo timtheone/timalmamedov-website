2de6222e5a1b7d856e744e7d013eda32cc881815
Tim Almamedov <tim.almamedov@gmail.com>
2018-08-02 18:13:09 +0200

    angular setup done, navbar, banner section done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { BioComponent } from './bio/bio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule
